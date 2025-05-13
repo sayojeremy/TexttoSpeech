70e97c624d8baf5052b691705206a56e7f4445ec
Sayo <sayomilembo@gmail.com>
2025-05-13 09:52:00 +0300

    Basic blueprint structure for text to speech app

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
+
     # Ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,24 @@
-from flask import render_template
-from . import main
+from wsgiref.util import request_uri
 
-@main.route("/")
+from flask import render_template, request, send_file, Response
+from . import main
+from gtts import gTTS
+import os
+import io
+
+
+@main.route("/", methods = ["POST", "GET"])
 def home():
+    if request.method == "POST":
+        text= request.form.get("text")
+        action = request.form.get("action")
+        tts = gTTS(text=text, lang='en')
+        mp3_fp = io.BytesIO()
+        tts.write_to_fp(mp3_fp)
+        mp3_fp.seek(0)
+
+        if action == "download":
+            return send_file(mp3_fp, as_attachment=True, download_name="speech.mp3", mimetype='audio/mpeg')
+        else:  # play
+            return send_file(mp3_fp, mimetype='audio/mpeg')
     return render_template("index.html")
